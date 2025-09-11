c6404aea98577aeb2a5f458d0438e01b08404f56
burk3405 <acburkett04@gmail.com>
2025-09-11 11:36:33 -0400

    Create horizontal.cpp

---
+++ Lecture4/horizontal.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	ofstream wf("outbin.ppm", ios::out | ios::binary);
+
+	string header = "P6 768 768  255 ";
+	// header = header + '\n';	// This line is irrelevant because of line 11
+	wf << header << endl;
+
+	int outer = 1, inner = 1;
+	int n, m, blok;
+	n = m = 768;
+	blok = 96;
+
+	int d1 = 255;
+	int d2 = 0;
+
+	for (int i = 0; i < n/96; i++) {
+		for (int j = 0; j < m ; j++) {
+			if (i % 2)
+				for (int a = 0; a < blok; a++) // For even Values- Green
+					wf << (char)255 << (char)0 << (char)0;
+					// wf.write((char*)&d1, 1);
+					// wf.write((char*)&d2, 1);
+					// wf.write((char*)&d2, 1);
+			else for (int a = 0; a < blok; a++) // For odd Values- Blue
+				wf << (char)0 << (char)255 << (char)0;
+				// wf.write((char*)&d2, 1);
+				// wf.write((char*)&d1, 1);
+				// wf.write((char*)&d2, 1);
+		}
+	}
+	cout << "Done" << endl;
+	system("pause");
+	return 0;
+}
